2291aa96a5b6a53f452a78e35fae70222351b797
Samar Shrestha <samars019370@nec.edu.np>
2022-11-12 16:35:27 +0545

    ADDED BUTTON SCREENSHOTS AND COORDINATES

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyautogui
 import time
 
+
 time.sleep(5)
-# print(pyautogui.position())
-im1 = pyautogui.screenshot(region=(867, 241, 200, 300))
-im1.save(r"C:\Users\lenev\OneDrive\Documents\pt_autofish\test1.png")
+print(pyautogui.position())
+# (x=1483, y=687) --  rod throw cordinates
+# (x=1493, y=756) -- position of jump/fish button
+# (867, 241) -- positon of ! mark on top of player model
+#im1 = pyautogui.screenshot(region=(1493, 756, 250, 250))
+# im1.save(r"C:\Users\lenev\OneDrive\Documents\pt_autofish\test2.png")
