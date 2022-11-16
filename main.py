b941eb57e1ec688270e2be47bec4877191956142
Samar Shrestha <samars019370@nec.edu.np>
2022-11-16 15:56:52 +0545

    Working atlantic and refined auto fish

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ time.sleep(5)
 # print(pyautogui.position())
 
 # load on what to search on ss
-fish_needle = cv.imread('fish.png', cv.IMREAD_COLOR)
+fish_needle = cv.imread('fish1.png', cv.IMREAD_COLOR)
 alert_needle = cv.imread('alert.png', cv.IMREAD_COLOR)
-jump_needle = cv.imread('jump.png', cv.IMREAD_COLOR)
+jump_needle = cv.imread('jump1.png', cv.IMREAD_COLOR)
+store_needle = cv.imread('store.png', cv.IMREAD_COLOR)
 
 rod = 0
 
@@ -27,15 +28,25 @@ def checkJump():
     return jmax_val
 
 
+def checkStore():
+    store_ss = ImageGrab.grab(bbox=(1268, 745, 1750, 913))
+    store_haystack = np.array(store_ss)
+    store_haystack = cv.cvtColor(store_haystack, cv.COLOR_RGB2BGR)
+    result_store = cv.matchTemplate(
+        store_haystack, store_needle, cv.TM_CCOEFF_NORMED)
+    smin_val, smax_val, smin_loc, smax_loc = cv.minMaxLoc(result_store)
+    return smax_val
+
+
 def fixRod():
     pyautogui.click(1749, 607)  # click the bag icon
     time.sleep(0.5)
     pyautogui.click(1370, 516)  # click repair icon
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.click(1040, 805)  # click the 500 money icon
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.click(833, 811)  # click yes
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.click(833, 811)  # get out of bag
     time.sleep(0.5)
 
@@ -43,7 +54,7 @@ def fixRod():
 while True:
     # grabbing ss of the fish and alert
     fish_ss = ImageGrab.grab(bbox=(1399, 688, 1825, 1100))
-    alert_ss = ImageGrab.grab(bbox=(902, 289, 964, 389))
+    alert_ss = ImageGrab.grab(bbox=(830, 311, 1040, 438))
 
     # converting to openCv readable format
     fish_haystack = np.array(fish_ss)
@@ -55,18 +66,19 @@ while True:
     result_fish = cv.matchTemplate(
         fish_haystack, fish_needle, cv.TM_CCOEFF_NORMED)
     result_alert = cv.matchTemplate(
-        alert_haystack, alert_needle, cv.TM_SQDIFF_NORMED)
+        alert_haystack, alert_needle, cv.TM_CCOEFF_NORMED)
     # get max match value -- for alert min gives most match
     amin_val, amax_val, amin_loc, amax_loc = cv.minMaxLoc(result_alert)
     fmin_val, fmax_val, fmin_loc, fmax_loc = cv.minMaxLoc(result_fish)
 
-    if (rod == 35):
+    if (rod == 36):
         fixRod()
         rod = 0
 
     if (fmax_val > 0.6):  # check if fish icon matches
         print("In Water !!")
-        if (amin_val < 0.2):  # check if fish is hooked
+        print(amax_val)
+        if (amax_val > 0.6):  # check if fish is hooked
             print("Fish hooked!!! Reel in Fast!!")
             pyautogui.click(1581, 827)  # click the reel out(fish) button
         else:
@@ -74,7 +86,7 @@ while True:
     elif (checkJump() > 0.6):
         print("Throwing hook in water!")
         pyautogui.click(1466, 693)  # click the throw hook button
-    else:
+    elif (checkStore() > 0.7):
         pyautogui.click(1429, 848)
         rod += 1
         time.sleep(0.5)
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
 import pyautogui
 from time import sleep
+import cv2 as cv
+import numpy as np
+from PIL import ImageGrab
 
 
 sleep(4)
 print(pyautogui.position())
+
+'''
+fish_needle = cv.imread('fish1.png', cv.IMREAD_COLOR)
+alert_needle = cv.imread('alert.png', cv.IMREAD_COLOR)
+jump_needle = cv.imread('jump1.png', cv.IMREAD_COLOR)
+
+fish_ss = ImageGrab.grab(bbox=(1399, 688, 1825, 1100))
+alert_ss = ImageGrab.grab(bbox=(848, 291, 1000, 397))
+fish_haystack = np.array(fish_ss)
+fish_haystack = cv.cvtColor(fish_haystack, cv.COLOR_RGB2BGR)
+alert_haystack = np.array(alert_ss)
+alert_haystack = cv.cvtColor(alert_haystack, cv.COLOR_RGB2BGR)
+
+result_fish = cv.matchTemplate(
+    fish_haystack, jump_needle, cv.TM_CCOEFF_NORMED)
+result_alert = cv.matchTemplate(
+    alert_haystack, alert_needle, cv.TM_CCOEFF_NORMED)
+
+amin_val, amax_val, amin_loc, amax_loc = cv.minMaxLoc(result_alert)
+fmin_val, fmax_val, fmin_loc, fmax_loc = cv.minMaxLoc(result_fish)
+
+print("alert value: ", amax_val)
+print("fish value: ", fmax_val)
+'''
